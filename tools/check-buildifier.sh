5d1e49cd6281ae31444af3ed2350176d9e3985f9
Dino Wernli <dinowernli@gmail.com>
2017-11-05 23:35:02 +0000

    Make bazel args explicit

--- tools/check-buildifier.sh
+++ tools/check-buildifier.sh
@@ -4,12 +4,9 @@ set -e
 
 if [[ "$1" == "--fix" ]]; then
   MODE="-mode=fix"
-  BAZEL_ARGS=""
 else
   MODE="-mode=check"
-  BAZEL_ARGS=$@
 fi
-
 
 bazel $BAZEL_ARGS build @com_github_bazelbuild_buildtools//buildifier:buildifier
 RESULT=`find -name BUILD -or -name WORKSPACE | xargs bazel-bin/external/com_github_bazelbuild_buildtools/buildifier/buildifier $MODE -v`
