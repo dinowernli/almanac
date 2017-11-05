88c9ef21a39c11244a6d11de94ff60f5e9cf980b
Dino Wernli <dinowernli@gmail.com>
2017-11-05 23:21:09 +0000

    Move argument around.

--- tools/check-buildifier.sh
+++ tools/check-buildifier.sh
@@ -11,7 +11,7 @@ else
 fi
 
 
-bazel build $BAZEL_ARGS @com_github_bazelbuild_buildtools//buildifier:buildifier
+bazel $BAZEL_ARGS build @com_github_bazelbuild_buildtools//buildifier:buildifier
 RESULT=`find -name BUILD -or -name WORKSPACE | xargs bazel-bin/external/com_github_bazelbuild_buildtools/buildifier/buildifier $MODE -v`
 
 # In theory, buildifier should return a non-zero exit code in check mode if there are errors. From
