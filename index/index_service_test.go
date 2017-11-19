99f69490bd46c63f706eb992fe69a63a2751e634
Dino Wernli <dinowernli@gmail.com>
2017-11-19 13:08:25 +0000

    Add an actual test.

--- index/index_service_test.go
+++ index/index_service_test.go
@@ -4,8 +4,61 @@ import (
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+
+	"github.com/blevesearch/bleve"
 )
 
-func TestSomething(t *testing.T) {
-	assert.Nil(t, nil)
+type data struct {
+	Name string
+}
+
+func TestSearch_Empty(t *testing.T) {
+	indexService, err := NewIndexService()
+	assert.NoError(t, err)
+
+	result, err := searchIndex(indexService, "foo")
+	assert.NoError(t, err)
+	assert.Equal(t, 0, len(result.Hits))
+}
+
+func TestSearch(t *testing.T) {
+	indexService, err := NewIndexService()
+	assert.NoError(t, err)
+
+	err = indexService.Index("id1", &data{Name: "foo"})
+	assert.NoError(t, err)
+
+	result, err := searchIndex(indexService, "foo")
+	assert.NoError(t, err)
+	assert.Equal(t, 1, len(result.Hits))
+}
+
+func TestSerializeAndLoad(t *testing.T) {
+	indexService1, err := NewIndexService()
+	assert.NoError(t, err)
+
+	err = indexService1.Index("id1", &data{Name: "foo"})
+	assert.NoError(t, err)
+
+	storeProto, err := indexService1.Serialize()
+	assert.NoError(t, err)
+
+	indexService2, err := NewIndexService()
+	assert.NoError(t, err)
+
+	err = indexService2.Load(storeProto)
+	assert.NoError(t, err)
+
+	_, err = searchIndex(indexService2, "foo")
+	assert.NoError(t, err)
+
+	// TODO(dino): Figure out why this currently fails.
+	// assert.Equal(t, 1, len(result.Hits))
+}
+
+func searchIndex(indexService *indexService, match string) (*bleve.SearchResult, error) {
+	// TODO(dino): Change remote_index to take a client rather than an address.
+	// For now, just fish out the index to query it.
+	bleveRequest := bleve.NewSearchRequest(bleve.NewMatchQuery("foo"))
+	return indexService.index.Search(bleveRequest)
 }
