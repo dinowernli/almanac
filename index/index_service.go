497c00c20e72c26f9cb446f476cb16c816184065
Dino Wernli <dinowernli@gmail.com>
2017-12-09 17:01:06 +0000

    Remove a bunch of stuff.

--- index/index_service.go
+++ index/index_service.go
@@ -63,19 +63,3 @@ func (s *indexService) Search(ctx context.Context, request *pb_logging.SearchReq
 	}
 	return &pb_logging.SearchResponse{BleveResponseBytes: bleveResultBytes}, nil
 }
-
-func (s *indexService) Serialize() (*pb_logging.Store, error) {
-	_, kvstore, err := s.index.Advanced()
-	if err != nil {
-		return nil, fmt.Errorf("unable to retrieve kvstore: %v", err)
-	}
-	return SerializeStore(kvstore)
-}
-
-func (s *indexService) Load(storeProto *pb_logging.Store) error {
-	_, kvstore, err := s.index.Advanced()
-	if err != nil {
-		return fmt.Errorf("unable to retrieve kvstore: %v", err)
-	}
-	return DeserializeStore(storeProto, kvstore)
-}
--- index/index_service_test.go
+++ index/index_service_test.go
@@ -32,29 +32,6 @@ func TestSearch(t *testing.T) {
 	assert.Equal(t, 1, len(result.Hits))
 }
 
-func TestSerializeAndLoad(t *testing.T) {
-	indexService1, err := NewIndexService()
-	assert.NoError(t, err)
-
-	err = indexService1.Index("id1", &data{Name: "foo"})
-	assert.NoError(t, err)
-
-	storeProto, err := indexService1.Serialize()
-	assert.NoError(t, err)
-
-	indexService2, err := NewIndexService()
-	assert.NoError(t, err)
-
-	err = indexService2.Load(storeProto)
-	assert.NoError(t, err)
-
-	_, err = searchIndex(indexService2, "foo")
-	assert.NoError(t, err)
-
-	// TODO(dino): Figure out why this currently fails.
-	// assert.Equal(t, 1, len(result.Hits))
-}
-
 func searchIndex(indexService *indexService, match string) (*bleve.SearchResult, error) {
 	// TODO(dino): Change remote_index to take a client rather than an address.
 	// For now, just fish out the index to query it.
