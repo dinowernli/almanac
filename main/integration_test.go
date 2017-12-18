35f1fa0081cca54c34cb29d5246fdb81d164dda1
Dino Wernli <dinowernli@gmail.com>
2017-12-18 12:45:29 +0000

    Create appender for test.

--- main/integration_test.go
+++ main/integration_test.go
@@ -2,8 +2,17 @@ package main
 
 import (
 	"testing"
+
+	"dinowernli.me/almanac/appender"
+	st "dinowernli.me/almanac/storage"
+
+	"github.com/stretchr/testify/assert"
 )
 
-func TestFoo(t *testing.T) {
+func TestFullSystem(t *testing.T) {
+	storage := st.NewInMemoryStorage()
 
+	entriesPerChunk := 3
+	_, err := appender.New("appender0", storage, entriesPerChunk)
+	assert.NoError(t, err)
 }
