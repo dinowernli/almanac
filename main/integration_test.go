2a9da0bb180528385c36b6d88020560b8cf62520
Dino Wernli <dinowernli@gmail.com>
2017-12-18 22:34:34 +0000

    Add test

--- main/integration_test.go
+++ main/integration_test.go
@@ -1,18 +1,57 @@
 package main
 
 import (
+	"fmt"
+	"net"
 	"testing"
 
 	"dinowernli.me/almanac/appender"
+	dc "dinowernli.me/almanac/discovery"
+	mx "dinowernli.me/almanac/mixer"
+	pb_almanac "dinowernli.me/almanac/proto"
 	st "dinowernli.me/almanac/storage"
 
 	"github.com/stretchr/testify/assert"
+	"google.golang.org/grpc"
 )
 
-func TestFullSystem(t *testing.T) {
+func TestIntegration(t *testing.T) {
+	entriesPerChunk := 10
+	numAppenders := 5
+	port := 51000
+
 	storage := st.NewInMemoryStorage()
 
-	entriesPerChunk := 3
-	_, err := appender.New("appender0", storage, entriesPerChunk)
-	assert.NoError(t, err)
+	appenders := []string{}
+	for i := 0; i < numAppenders; i++ {
+		appender, err := appender.New(fmt.Sprintf("appender-%d", i), storage, entriesPerChunk)
+		assert.NoError(t, err)
+
+		address := fmt.Sprintf("localhost:%d", port)
+		port++
+
+		err = startServer(appender, address)
+		assert.NoError(t, err)
+		appenders = append(appenders, address)
+	}
+
+	discovery := dc.New(appenders)
+	mixer := mx.New(storage, discovery)
+
+	mixer.Search(nil, nil)
+}
+
+func startServer(appender *appender.Appender, address string) error {
+	listen, err := net.Listen("tcp", address)
+	if err != nil {
+		return fmt.Errorf("failed to listen on port %d: %v", port, err)
+	}
+
+	server := grpc.NewServer()
+	pb_logging.RegisterAppenderServer(server, appender)
+	go func() {
+		server.Serve(listen)
+	}()
+
+	return nil
 }
