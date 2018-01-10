f6c10384849a2f005fc657e6f2616438afadad28
Dino Wernli <dinowernli@gmail.com>
2018-01-10 19:16:36 +0000

    Make code gen more fancy.

--- tools/genproto.sh
+++ tools/genproto.sh
@@ -5,4 +5,8 @@ set -e
 ALMANAC_ROOT=$GOPATH/src/dinowernli.me/almanac
 PROTO_DIR=$ALMANAC_ROOT/proto
 
-protoc --go_out=$ALMANAC_ROOT --go_out=plugins=grpc:. --proto_path=$ALMANAC_ROOT $PROTO_DIR/*.proto
+protoc \
+  --go_out=$ALMANAC_ROOT \
+  --go_out=plugins=grpc:. \
+  --proto_path=$ALMANAC_ROOT \
+  $PROTO_DIR/*.proto
