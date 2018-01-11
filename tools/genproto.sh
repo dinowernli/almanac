f3f4c0e150b816295502f32d9dd9ffcd59e471c2
Dino Wernli <dinowernli@gmail.com>
2018-01-11 10:51:30 +0000

    More travis massaging...

--- tools/genproto.sh
+++ tools/genproto.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
-set -e
+set -x
 
 ALMANAC_ROOT=$GOPATH/src/dinowernli.me/almanac
 PROTO_DIR=$ALMANAC_ROOT/proto
 
-BEFORESUM=`find $PROTO_DIR -type f -exec shasum {} \; | shasum`
+TMPDIR=`mktemp -d`
+BACKUP_ROOT=$TMPDIR/almanac
+cp -R $ALMANAC_ROOT $BACKUP_ROOT
+BACKUP_PROTO_DIR=$BACKUP_ROOT/proto
 
-rm $PROTO_DIR/*.pb.go
 protoc \
   --go_out=$ALMANAC_ROOT \
   --go_out=plugins=grpc:. \
   --proto_path=$ALMANAC_ROOT \
   $PROTO_DIR/*.proto
+PROTOC_OUT=$?
 
-AFTERSUM=`find $PROTO_DIR -type f -exec shasum {} \; | shasum`
+DIFF=`diff -rq $BACKUP_PROTO_DIR $PROTO_DIR`
 
+rm -rf $TMPDIR
+
+# Definitely error out if protoc failed.
+if [[ $PROTOC_OUT -ne 0 ]]; then
+  echo 'protoc failed'
+  exit 1
+fi
+
+# Error out in check mode if the diff didn't match.
 if [[ "$1" == "--check" ]]; then
-  if [ "$BEFORESUM" == "$AFTERSUM" ]; then
-    echo 'check succeeded, directories were the same'
+  if [[ -z $DIFF ]]; then
+    echo "check succeeded, directories were the same"
     exit 0
   else
-    echo 'check failed, directories were different'
-    echo 'please rerun with --check' 
+    echo "check failed, directories were different"
+    echo "diff: $DIFF"
     exit 1
   fi
 fi
