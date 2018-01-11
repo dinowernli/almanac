ddc5a41d615d55d46aa545bb5e3f1961de2be60d
Dino Wernli <dinowernli@gmail.com>
2018-01-11 13:19:26 +0000

    Split packages apart.

--- test/integration_test.go
+++ test/integration_test.go
@@ -1,10 +1,11 @@
-package main
+package test
 
 import (
 	"encoding/json"
 	"fmt"
 	"testing"
 
+	"dinowernli.me/almanac/pkg/cluster"
 	pb_almanac "dinowernli.me/almanac/proto"
 
 	"github.com/sirupsen/logrus"
@@ -18,97 +19,103 @@ const (
 )
 
 var (
-	testConf = &config{
-		smallChunkMaxEntries: 10,
-		smallChunkSpreadMs:   5000,
-		smallChunkMaxAgeMs:   3000,
+	testConf = &cluster.Config{
+		SmallChunkMaxEntries: 10,
+		SmallChunkSpreadMs:   5000,
+		SmallChunkMaxAgeMs:   3000,
 
-		storageType: storageTypeMemory,
-		gcsBucket:   "",
+		StorageType: "memory",
+		GcsBucket:   "",
 	}
 )
 
+type entry struct {
+	Message     string `json:"message"`
+	Logger      string `json:"logger"`
+	TimestampMs int64  `json:"timestamp_ms"`
+}
+
 func TestNoEntries(t *testing.T) {
 	c := createTestCluster(t)
-	defer c.stop()
+	defer c.Stop()
 
 	request := &pb_almanac.SearchRequest{Num: 200, Query: "foo"}
-	response, err := c.mixer.Search(context.Background(), request)
+	response, err := c.Mixer.Search(context.Background(), request)
 	assert.NoError(t, err)
 	assert.Equal(t, 0, len(response.Entries))
 }
 
 func TestSearchesAppenders(t *testing.T) {
 	c := createTestCluster(t)
-	defer c.stop()
+	defer c.Stop()
 
 	// Add an entry containing "foo" to the first appender.
 	append1, err := appendRequest("id1", "foo", 123)
 	assert.NoError(t, err)
 
-	_, err = c.appenders[0].Append(context.Background(), append1)
+	_, err = c.Appenders[0].Append(context.Background(), append1)
 	assert.NoError(t, err)
 
 	// Add a different entry containing "foo" to another appender.
 	append2, err := appendRequest("id2", "foo", 567)
 	assert.NoError(t, err)
 
-	_, err = c.appenders[2].Append(context.Background(), append2)
+	_, err = c.Appenders[2].Append(context.Background(), append2)
 	assert.NoError(t, err)
 
 	// Add an entry which does not contain "foo".
 	append3, err := appendRequest("id3", "baz", 789)
 	assert.NoError(t, err)
 
-	_, err = c.appenders[1].Append(context.Background(), append3)
+	_, err = c.Appenders[1].Append(context.Background(), append3)
 	assert.NoError(t, err)
 
 	// Make sure we get two hits when we search for foo.
 	request := &pb_almanac.SearchRequest{Num: 200, Query: "foo"}
-	response, err := c.mixer.Search(context.Background(), request)
+	response, err := c.Mixer.Search(context.Background(), request)
 	assert.NoError(t, err)
 	assert.Equal(t, 2, len(response.Entries))
 }
 
 func TestRoundTripThroughStorage(t *testing.T) {
 	c := createTestCluster(t)
-	defer c.stop()
+	defer c.Stop()
 
 	// Add multiple chunks worth of entries, try to make sure we have an
 	// open chunk as well.
-	numEntries := 10*testConf.smallChunkMaxEntries + 1
+	numEntries := 10*testConf.SmallChunkMaxEntries + 1
 	for i := 0; i < numEntries; i++ {
 		request, err := appendRequest(fmt.Sprintf("id-%d", i), "foo", 123)
 		assert.NoError(t, err)
 
-		_, err = c.appenders[0].Append(context.Background(), request)
+		_, err = c.Appenders[0].Append(context.Background(), request)
 		assert.NoError(t, err)
 	}
 
 	// Make sure all entries turn up.
 	request := &pb_almanac.SearchRequest{Num: 200, Query: "foo"}
-	response, err := c.mixer.Search(context.Background(), request)
+	response, err := c.Mixer.Search(context.Background(), request)
 	assert.NoError(t, err)
 	assert.Equal(t, numEntries, len(response.Entries))
 }
 
 func TestDeduplicatesEntries(t *testing.T) {
 	c := createTestCluster(t)
-	defer c.stop()
+	defer c.Stop()
 
 	// Add an entry containing "foo" to multiple appenders.
 	append1, err := appendRequest("id1", "foo", 123)
 	assert.NoError(t, err)
 
-	_, err = c.appenders[0].Append(context.Background(), append1)
+	_, err = c.Appenders[0].Append(context.Background(), append1)
 	assert.NoError(t, err)
 
-	_, err = c.appenders[1].Append(context.Background(), append1)
+	_, err = c.Appenders[1].Append(context.Background(), append1)
 	assert.NoError(t, err)
 
 	// Now make sure that a search for foo only returns the entry once.
 	request := &pb_almanac.SearchRequest{Num: 200, Query: "foo"}
-	response, err := c.mixer.Search(context.Background(), request)
+	response, err := c.Mixer.Search(context.Background(), request)
 	assert.NoError(t, err)
 
 	assert.Equal(t, 1, len(response.Entries))
@@ -116,22 +123,22 @@ func TestDeduplicatesEntries(t *testing.T) {
 
 func TestIngestsEntry(t *testing.T) {
 	c := createTestCluster(t)
-	defer c.stop()
+	defer c.Stop()
 
 	// Ship an entry with a predefined timestamp.
 	ingestRequest1, err := newIngestRequest(&entry{Message: "foo", TimestampMs: 5000})
 	assert.NoError(t, err)
-	_, err = c.ingester.Ingest(context.Background(), ingestRequest1)
+	_, err = c.Ingester.Ingest(context.Background(), ingestRequest1)
 	assert.NoError(t, err)
 
 	// Not timestamp for this entry, the ingester should infer it.
 	ingestRequest2, err := newIngestRequest(&entry{Message: "foo"})
 	assert.NoError(t, err)
-	_, err = c.ingester.Ingest(context.Background(), ingestRequest2)
+	_, err = c.Ingester.Ingest(context.Background(), ingestRequest2)
 	assert.NoError(t, err)
 
 	request := &pb_almanac.SearchRequest{Num: 200, Query: "foo"}
-	response, err := c.mixer.Search(context.Background(), request)
+	response, err := c.Mixer.Search(context.Background(), request)
 	assert.NoError(t, err)
 
 	assert.Equal(t, 2, len(response.Entries))
@@ -146,23 +153,23 @@ func TestIngestsEntry(t *testing.T) {
 
 func TestQueryRange(t *testing.T) {
 	c := createTestCluster(t)
-	defer c.stop()
+	defer c.Stop()
 
 	ingestRequest1, err := newIngestRequest(&entry{Message: "foo", TimestampMs: 5000})
 	assert.NoError(t, err)
-	_, err = c.ingester.Ingest(context.Background(), ingestRequest1)
+	_, err = c.Ingester.Ingest(context.Background(), ingestRequest1)
 	assert.NoError(t, err)
 
 	// Issue a query for a range which does not contain the value above.
 	request := &pb_almanac.SearchRequest{Num: 200, Query: "foo", StartMs: 3000, EndMs: 4000}
-	response, err := c.mixer.Search(context.Background(), request)
+	response, err := c.Mixer.Search(context.Background(), request)
 	assert.NoError(t, err)
 
 	assert.Equal(t, 0, len(response.Entries))
 }
 
-func createTestCluster(t *testing.T) *localCluster {
-	c, err := createCluster(logrus.New(), testConf, getAppenderPorts(), appenderFanout)
+func createTestCluster(t *testing.T) *cluster.LocalCluster {
+	c, err := cluster.CreateCluster(logrus.New(), testConf, getAppenderPorts(), appenderFanout)
 	assert.NoError(t, err)
 	return c
 }
@@ -189,4 +196,12 @@ func appendRequest(id string, message string, timestampMs int64) (*pb_almanac.Ap
 			Id:          id,
 		},
 	}, nil
+}
+
+func newIngestRequest(e interface{}) (*pb_almanac.IngestRequest, error) {
+	entryJson, err := json.Marshal(e)
+	if err != nil {
+		return nil, fmt.Errorf("unable to marshal entry to json: %v", err)
+	}
+	return &pb_almanac.IngestRequest{EntryJson: string(entryJson)}, nil
 }
