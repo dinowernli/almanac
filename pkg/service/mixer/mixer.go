15105642fbf04cd623a5424387efc3c9096bf9a3
Dino Wernli <dinowernli@gmail.com>
2018-01-15 17:08:51 +0000

    Implement a smarter search algorithm in the query mixer (#58)

--- pkg/service/mixer/mixer.go
+++ pkg/service/mixer/mixer.go
@@ -1,9 +1,9 @@
 package mixer
 
 import (
+	"container/heap"
 	"fmt"
 	"net/http"
-	"sort"
 	"time"
 
 	almHttp "dinowernli.me/almanac/pkg/http"
@@ -13,6 +13,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
+	"golang.org/x/sync/errgroup"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 )
@@ -49,109 +50,91 @@ func (m *Mixer) RegisterHttp(server *http.ServeMux) {
 
 func (m *Mixer) Search(ctx context.Context, request *pb_almanac.SearchRequest) (*pb_almanac.SearchResponse, error) {
 	logger := m.logger.WithFields(searchField)
-
 	if request.StartMs != 0 && request.EndMs != 0 && request.StartMs > request.EndMs {
 		err := grpc.Errorf(codes.InvalidArgument, "cannot query, start(%d) is greater than end (%d)", request.StartMs, request.EndMs)
 		logger.WithError(err).Warnf("Failed")
 		return nil, err
 	}
 
-	// Do some prep for the parallel searches.
-	appenders := m.discovery.ListAppenders()
-	chunkIds, err := m.storage.ListChunks(request.StartMs, request.EndMs)
-	if err != nil {
-		err := grpc.Errorf(codes.Internal, "unable to list chunks: %v", err)
-		logger.WithError(err).Warnf("Failed")
-		return nil, err
-	}
+	searchHeap := &searchHeap{}
+	heap.Init(searchHeap)
+	g, _ := errgroup.WithContext(ctx)
 
-	// Execute all the searches in parallel.
-	numSubRequests := len(appenders) + len(chunkIds)
-	resultChan := make(chan *partialResult, numSubRequests)
-	for _, chunkId := range chunkIds {
-		go m.searchChunk(ctx, chunkId, request, resultChan)
-	}
-	for _, appender := range appenders {
-		go m.searchAppender(ctx, appender, request, resultChan)
-	}
-
-	// Drain the channel and collect all the entries.
-	allEntries := []*pb_almanac.LogEntry{}
-	err = nil
-	for i := 0; i < numSubRequests; i++ {
-		result := <-resultChan
-		if result.chunk != nil {
-			result.chunk.Close()
-		}
-
-		if result.err == nil {
-			for _, e := range result.entries {
-				allEntries = append(allEntries, e)
+	// Compute one heap item for every appender.
+	for _, a := range m.discovery.ListAppenders() {
+		// Copy the iteration variable here because otherwise, all instances of the func below end up
+		// using the same appender because golang loop variables are by reference.
+		appender := a
+		g.Go(func() error {
+			response, err := appender.Search(ctx, request)
+			if err != nil {
+				return fmt.Errorf("unable to search appender: %v", err)
 			}
-		} else {
-			err = result.err
-		}
+			if len(response.Entries) > 0 {
+				heap.Push(searchHeap, &appenderHeapItem{entries: response.Entries, idx: 0})
+			}
+			return nil
+		})
 	}
 
-	if err != nil {
-		err := grpc.Errorf(codes.Internal, "error executing search: %v", err)
+	// Compute a heap item for every chunk whose time span overlaps with our query.
+	g.Go(func() error {
+		chunkIds, err := m.storage.ListChunks(request.StartMs, request.EndMs)
+		if err != nil {
+			return fmt.Errorf("unable to list chunks: %v", err)
+		}
+		for _, id := range chunkIds {
+			idProto, err := storage.ChunkIdProto(id)
+			if err != nil {
+				return fmt.Errorf("unable to compute chunk id proto: %v", err)
+			}
+			heap.Push(searchHeap, &chunkHeapItem{chunkIdProto: idProto, searchRequest: request, ctx: ctx, storage: m.storage})
+		}
+		return nil
+	})
+
+	if err := g.Wait(); err != nil {
+		err := grpc.Errorf(codes.Internal, "search failed: %v", err)
 		logger.WithError(err).Warnf("Failed")
 		return nil, err
 	}
 
-	// Sort all the entries by timestamp and take the first "num" distinct ones.
-	sort.Sort(byTimestamp(allEntries))
+	// Start assembling results by repeatedly grabbing the next one from the heap.
 	result := []*pb_almanac.LogEntry{}
 	seen := map[string]struct{}{}
-	for _, entry := range allEntries {
-		if _, ok := seen[entry.Id]; ok {
-			continue
+	for searchHeap.Len() > 0 {
+		item := heap.Pop(searchHeap).(heapItem)
+		entry, err := item.entry()
+		if err != nil {
+			err := grpc.Errorf(codes.Internal, "unable to extract entry from heap item: %v", err)
+			logger.WithError(err).Warnf("Failed")
+			return nil, err
 		}
-		seen[entry.Id] = struct{}{}
 
-		result = append(result, entry)
-		if len(result) >= int(request.Num) {
-			break
+		// Incorporate the entry into our result set (including deduping).
+		if _, ok := seen[entry.Id]; !ok {
+			seen[entry.Id] = struct{}{}
+			result = append(result, entry)
+			if len(result) >= int(request.Num) {
+				break
+			}
+		}
+
+		// Re-populate the heap if necessary.
+		next, err := item.next()
+		if err != nil {
+			err := grpc.Errorf(codes.Internal, "unable to extract next item from heap item: %v", err)
+			logger.WithError(err).Warnf("Failed")
+			return nil, err
+		}
+		if next != nil {
+			heap.Push(searchHeap, next)
 		}
 	}
-	logger = logger.WithFields(logrus.Fields{"hits": len(result)})
 
+	logger = logger.WithFields(logrus.Fields{"hits": len(result)})
 	logger.Infof("Handled")
 	return &pb_almanac.SearchResponse{Entries: result}, nil
-}
-
-// searchChunk performs a search on a single chunk and pipes the result into
-// the supplied channel.
-func (m *Mixer) searchChunk(ctx context.Context, chunkId string, request *pb_almanac.SearchRequest, resultChan chan *partialResult) {
-	result := &partialResult{}
-	chunk, err := m.storage.LoadChunk(chunkId)
-	if err != nil {
-		result.err = fmt.Errorf("unable to load chunk %s: %v", chunkId, err)
-		resultChan <- result
-		return
-	}
-	result.chunk = chunk
-
-	entries, err := chunk.Search(ctx, request.Query, request.Num, request.StartMs, request.EndMs)
-	if err != nil {
-		result.err = fmt.Errorf("unable to perform search on chunk %s: %v", chunkId, err)
-		resultChan <- result
-		return
-	}
-
-	result.entries = entries
-	resultChan <- result
-}
-
-// searchApender performs a search on a single appender and pipes the result into
-// the supplied channel.
-func (m *Mixer) searchAppender(ctx context.Context, appender pb_almanac.AppenderClient, request *pb_almanac.SearchRequest, resultChan chan *partialResult) {
-	response, err := appender.Search(ctx, request)
-	if err != nil {
-		resultChan <- &partialResult{err: fmt.Errorf("unable to search appender: %v", err)}
-		return
-	}
-	resultChan <- &partialResult{entries: response.Entries}
 }
 
 // handleHttp serves a web page which can be used to execute queries on this mixer.
@@ -185,15 +168,3 @@ func (m *Mixer) handleHttp(writer http.ResponseWriter, request *http.Request) {
 		fmt.Fprintf(writer, "failed to render mixer page: %v", err)
 	}
 }
-
-type partialResult struct {
-	chunk   *storage.Chunk
-	entries []*pb_almanac.LogEntry
-	err     error
-}
-
-type byTimestamp []*pb_almanac.LogEntry
-
-func (h byTimestamp) Len() int           { return len(h) }
-func (h byTimestamp) Less(i, j int) bool { return h[i].TimestampMs < h[j].TimestampMs }
-func (h byTimestamp) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
---
+++ pkg/service/mixer/mixer_test.go
@@ -0,0 +1,57 @@
+package mixer
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+
+	"dinowernli.me/almanac/pkg/service/discovery"
+	st "dinowernli.me/almanac/pkg/storage"
+	pb_almanac "dinowernli.me/almanac/proto"
+
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+)
+
+func TestMixerCallsAppenders(t *testing.T) {
+	appenders := []pb_almanac.AppenderClient{&fakeAppender{}, &fakeAppender{}, &fakeAppender{}, &fakeAppender{}}
+	mixer := New(logrus.New(), st.NewInMemoryStorage(), discovery.NewForTesting(appenders))
+
+	_, err := mixer.Search(context.Background(), &pb_almanac.SearchRequest{})
+	assert.NoError(t, err)
+
+	for _, appender := range appenders {
+		assert.Equal(t, 1, appender.(*fakeAppender).searchCalls)
+	}
+}
+
+func TestHttp(t *testing.T) {
+	appenders := []pb_almanac.AppenderClient{&fakeAppender{}}
+	mixer := New(logrus.New(), st.NewInMemoryStorage(), discovery.NewForTesting(appenders))
+
+	request, err := http.NewRequest("GET", "/mixer", nil)
+	assert.NoError(t, err)
+
+	recorder := httptest.NewRecorder()
+	mixer.handleHttp(recorder, request)
+	assert.Equal(t, http.StatusOK, recorder.Code)
+
+	// Cheap way to check that the template rendered correctly.
+	assert.True(t, strings.Contains(recorder.Body.String(), "Mixer"))
+}
+
+type fakeAppender struct {
+	searchCalls int
+}
+
+func (a *fakeAppender) Search(ctx context.Context, request *pb_almanac.SearchRequest, options ...grpc.CallOption) (*pb_almanac.SearchResponse, error) {
+	a.searchCalls++
+	return &pb_almanac.SearchResponse{}, nil
+}
+
+func (a *fakeAppender) Append(ctx context.Context, request *pb_almanac.AppendRequest, options ...grpc.CallOption) (*pb_almanac.AppendResponse, error) {
+	return &pb_almanac.AppendResponse{}, nil
+}
